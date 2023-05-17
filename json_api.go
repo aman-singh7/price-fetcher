413875e94cadccec3f899c68ab70e0015e8c678a
Aman <aman2@me.iitr.ac.in>
2023-05-18 01:02:12 +0530

    feat: implement json server to fetch prices
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

---
+++ json_api.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+
+	"github.com/aman-singh7/price-fetcher/types"
+)
+
+type JSONAPIServer struct {
+	listenAddr string
+	svc        PriceFetcher
+}
+
+func NewJSONAPIServer(listenAddr string, svc PriceFetcher) *JSONAPIServer {
+	return &JSONAPIServer{
+		listenAddr: listenAddr,
+		svc:        svc,
+	}
+}
+
+func (s *JSONAPIServer) Run() {
+	http.HandleFunc("/", makeHTTPHandlerFunc(s.handleFetchPrice))
+
+	http.ListenAndServe(s.listenAddr, nil)
+}
+
+type APIFunc func(context.Context, http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandlerFunc(apiFn APIFunc) http.HandlerFunc {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "requestID", rand.Intn(10000000))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := apiFn(ctx, w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+	}
+}
+
+func (s *JSONAPIServer) handleFetchPrice(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	ticker := r.URL.Query().Get("ticker")
+
+	price, err := s.svc.FetchPrice(ctx, ticker)
+	if err != nil {
+		return err
+	}
+
+	priceResp := types.PriceResponse{
+		Price:  price,
+		Ticker: ticker,
+	}
+
+	return writeJSON(w, http.StatusOK, priceResp)
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
